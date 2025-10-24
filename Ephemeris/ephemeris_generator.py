48a769998aa9da2f55f3130fec78d888b1914786
joii9 <joel.c.mz9@gmail.com>
2025-10-24 17:57:24 -0600

    I had to divide the Ephemeris file because I will use some methods of Ephemeris.py in Argument File Generator EWSK Single Part

---
+++ Ephemeris/ephemeris_generator.py
@@ -0,0 +1,10 @@
+from Ephemeris import Ephemeris
+
+ephemeris_generator= Ephemeris()
+ephemeris_generator.current_date()
+ephemeris_generator.request_dateTime()
+ephemeris_generator.user_comments()
+ephemeris_generator.get_file()
+ephemeris_generator.find_date()
+ephemeris_generator.convert_date_J2000()
+ephemeris_generator.generate_file()
